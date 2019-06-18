e5e72c47c52e26158316fb8763fbe65249202753
Dan Shai <danshai2013@gmail.com>
2019-06-18 16:34:36 +0300

    Updates

---
+++ precache-manifest.33603b7cdc92d5adbe34a66fcec814e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "936a07a7a5c591d51295c533f95b58d8",
+    "url": "/dan/index.html"
+  },
+  {
+    "revision": "916a1ecb83b078f510b7",
+    "url": "/dan/static/css/main.2b57c385.chunk.css"
+  },
+  {
+    "revision": "3d29d6f8e428814aa815",
+    "url": "/dan/static/js/2.363e7c7d.chunk.js"
+  },
+  {
+    "revision": "916a1ecb83b078f510b7",
+    "url": "/dan/static/js/main.a94b0ece.chunk.js"
+  },
+  {
+    "revision": "c857ce885ced8bb23c46",
+    "url": "/dan/static/js/runtime~main.0cfc3a23.js"
+  }
+]);
